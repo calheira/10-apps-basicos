e6396a48e6557daada3789c6e1d6fdac3b4ae64c
Bruno Calheira <bruno@calheira.com>
2019-04-25 18:28:42 -0300

    App 01

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 10-apps-basicos
+
 Dez aplicativos básicos para emprender Python
+
+
+## Apps
+
+01 - Oi Fulano
+
---
+++ apps/01_oi_fulano/programa.py
@@ -0,0 +1,14 @@
+"""
+Aplicativos Simples para Aprender Python
+App 01
+Aplicativo que diz: "Oi!"
+Conceitos: #variaveis, #comentarios, #print,
+#strings, #formatacao, #python-em-pt-br
+Autor: Bruno Calheira 
+"""
+print("-"*10)
+print("Oi Fulano!")
+print("-"*10)
+print("\n")
+nome_do_fulano = input("Qual o seu nome? ")
+print(f"Oi {nome_do_fulano}!\nÉ um prazer conhecer você.")
